1d6a6ed3786b628a22c04b7ba72f9687146f8c1b
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-12 09:06:30 +0100

    Second question for the day

---
+++ strict-equality-operator.js
@@ -0,0 +1,26 @@
+//Question
+// Use the strict equality operator in the if statement 
+// so the function will return "Equal" when val is strictly equal to 7
+
+// // Setup
+// function testStrict(val) {
+//   if (val === 7) { // Change this line
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+
+// // Change this value to test
+// testStrict(10);
+
+// Answer
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
