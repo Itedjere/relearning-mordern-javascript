4d16c3d5aad5d2ee2df2e9b758b1f55f55c0165a
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-13 11:07:48 +0100

    6th challenge for the day

---
+++ arrow-functions-with-parameters.js
@@ -0,0 +1,11 @@
+// var myConcat = function(arr1, arr2) {
+//   "use strict";
+//   return arr1.concat(arr2);
+// };
+// // test your code
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
