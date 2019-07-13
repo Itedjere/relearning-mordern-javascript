e276ffe471f1a961da417ed3c467e8238b2c665c
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-13 10:36:27 +0100

    First challenge for today

---
+++ var-and-let-keywords.js
@@ -0,0 +1,25 @@
+//Questions
+//Update the code so it only uses the let keyword.
+
+// var catName;
+// var quote;
+// function catTalk() {
+//   "use strict";
+
+//   catName = "Oliver";
+//   quote = catName + " says Meow!";
+
+// }
+// catTalk();
+
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
