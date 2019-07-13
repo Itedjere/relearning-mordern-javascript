586642e71f56d03984f6d31ae7c3fab6d336d454
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-13 11:04:59 +0100

    5th challenge for the day

---
+++ arrow-functions.js
@@ -0,0 +1,8 @@
+//Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also make sure nothing is defined using the keyword var.
+
+// var magic = function() {
+//   "use strict";
+//   return new Date();
+// };
+
+const magic = () => new Date();
