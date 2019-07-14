24e0751ce1c59d581d4e32cecbc8311f7a99d5f4
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-14 23:34:01 +0100

    6th challange for the day

---
+++ export.js
@@ -0,0 +1,5 @@
+// Below are two variables that I want to make available for other files to use. Utilizing the first way I demonstrated export, export the two variables.
+
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
