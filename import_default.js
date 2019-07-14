554258400dae23f5835fd110e38cc39555df4eee
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-14 23:42:58 +0100

    7th challenge for today

---
+++ import_default.js
@@ -0,0 +1,17 @@
+// In the following code, please import the default export, subtract, from the file "math_functions", found in the same directory as this file.
+
+
+// "use strict";
+// subtract(7,4);
+
+
+
+
+
+
+
+
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
