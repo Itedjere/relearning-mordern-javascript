3805d13a01490d746ef5d133042082952ee6873e
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-14 23:40:13 +0100

    7th challenge today

---
+++ export_default.js
@@ -0,0 +1,9 @@
+// The following function should be the fallback value for the module. Please add the necessary code to do so.
+
+// "use strict";
+// function subtract(x,y) {return x - y;}
+
+
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
