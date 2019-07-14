dd37ebd389334ad69e812f7539da6d4334285bb7
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-14 23:30:29 +0100

    5th challenge today

---
+++ import-require-difference.js
@@ -0,0 +1,16 @@
+// Add the appropriate import statement that will allow the current file to use the capitalizeString function. The file where this function lives is called "string_functions", and it is in the same directory as the current file.
+
+
+// "use strict";
+
+// capitalizeString("hello!");
+
+
+
+
+
+"use strict";
+
+import { capitalizeString } from './string_functions'
+
+capitalizeString("hello!");
