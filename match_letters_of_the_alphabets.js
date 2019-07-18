30be4fba5734d70937fed71166e2095eed70f9da
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-18 10:18:47 +0100

    8th challenge for the day. Final one

---
+++ match_letters_of_the_alphabets.js
@@ -0,0 +1,15 @@
+// Match all the letters in the string quoteSample.
+
+
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /change/; // Change this line
+// let result = alphabetRegex; // Change this line
+
+
+
+
+
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
