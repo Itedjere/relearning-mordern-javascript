cd1a309cf4a949447fa7cc54023d560c26d44e9c
Itedjere Godspower <itedjereg77@gmail.com>
2019-07-19 18:08:26 +0100

    2nd challenge today

---
+++ regex_negated_character_Set.js
@@ -0,0 +1,10 @@
+// Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+
+// let quoteSample = "3 blind mice.";
+// let myRegex = /change/; // Change this line
+// let result = myRegex; // Change this line
+
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou\d]/ig; // Change this line
+let result = myRegex; // Change this line
